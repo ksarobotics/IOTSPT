615cd3fa1e262e011dfecb90fdeed699b25f1b67
KSA Robotics <awaiskhawar@yahoo.com>
2019-11-03 22:22:56 +0500

    Add files via upload

---
+++ examples/Simple/Simple.ino
@@ -0,0 +1,27 @@
+
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+int httpCode =0; 
+char ssid[]="PTCL-BB";
+char password[]="totalmart1";
+String email="awaiskhawar@yahoo.com";
+String key="9283767126";
+String downloadedData;
+
+void setup() {
+ 
+  Serial.begin(9600);
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for the WiFI connection completion
+    delay(500);
+    Serial.println("Waiting for connection");
+       } 
+       upload(3, "1234");
+       downloadedData=download(2);
+       Serial.println(downloadedData);
+}
+
+void loop(){
+  
+  }
+ 
